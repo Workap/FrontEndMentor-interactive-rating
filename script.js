ddd37dea17e7bfd1d26665b6fcf2d62d566a50bf
Workap <soyh629@gmail.com>
2023-06-03 16:50:04 -0500

    Thank you FrontEndMentor/lottie file Animation

---
+++ script.js
@@ -0,0 +1,41 @@
+const submit = document.querySelector("#submit");
+let rate;
+const rateNum = (e) => {
+  rate = e.target.innerText;
+  if (rate.length > 1) {
+    rate = "5";
+  }
+  console.log(rate);
+  return rate;
+};
+
+const thanksYouCard = () => {
+  const container = document.querySelector(".container__card");
+  container.innerHTML = `
+  <div class="lottie__animation">
+  <lottie-player
+    src="images/illustration-thank.json"
+    background="transparent"
+    speed=".5"
+    autoplay
+  ></lottie-player>
+</div>
+<div class="container__card-rate--slect">
+  <p>You selected ${rate} out of 5</p>
+</div>
+<div class="container__card-title h1-thank"><h1>Thank you!</h1></div>
+<div class="container__card-description p-thank">
+  <p>
+    We appreciate you taking the time to give a rating. If you ever need
+    more support, don’t hesitate to get in touch!
+  </p>
+</div>
+  `;
+  console.log(rate);
+};
+
+document
+  .querySelectorAll(".container__card-rate")
+  .forEach((container) => container.addEventListener("click", rateNum));
+
+submit.addEventListener("click", thanksYouCard);
